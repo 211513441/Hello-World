d971e84ab4ca1ad769594f39d6e5010f42b677ac
Abdulaziz Alnaim <45672672+211513441@users.noreply.github.com>
2021-01-19 06:33:43 +0300

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int length,wedth,Area;
+printf("Enter length of rectangul\n");
+scanf("%d",&length);
+printf("Enter wedth of rectangul\n");
+scanf("%d",&wedth);
+Area=length*wedth;
+printf("Area of rectangul is %d",Area);
+}
